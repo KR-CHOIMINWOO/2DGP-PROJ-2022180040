afdae4d45be87c0a40803bd657d3720c37e960b0
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-11-09 23:58:27 +0900

    용사에 프레임 기반 속도 계산 적용 준비

--- 2DGP-PROJ-Python-2022180040/tuar2.py
+++ 2DGP-PROJ-Python-2022180040/tuar2.py
@@ -22,6 +22,22 @@ def up_up(e):       return e[0] == 'INPUT' and e[1].type == SDL_KEYUP   and e[1]
 def down_down(e):   return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_DOWN
 def down_up(e):     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP   and e[1].key == SDLK_DOWN
 
+# 용사의 Run Speed 계산
+
+# 용사 Run Speed
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 20.0  # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+# 용사 Action Speed
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+
+
 class Idle:
 
     def __init__(self, tuar):
