d95a72c9d5d8548c23d86ebfd1a549f2c58fb5f7
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-11-10 23:50:13 +0900

    UI별 경로 저장

--- 2DGP-PROJ-Python-2022180040/draw_ui.py
+++ 2DGP-PROJ-Python-2022180040/draw_ui.py
@@ -4,3 +4,10 @@ import math
 
 _CW, _CH = 1280, 720
 
+IMG_HP_FILL   = 'image_file/effect/UI/effect_font_0025_life_bar.png'
+IMG_HP_FRAME  = 'image_file/effect/UI/effect_font_0024_life_empty.png'
+IMG_HP_TEXT   = 'image_file/effect/UI/effect_font_0003_HP.png'
+IMG_ICON_TUAR = 'image_file/effect/UI/icon_tuar.png'
+IMG_BTN_ROLL  = 'image_file/effect/UI/button_roll.png'
+IMG_AWAKEN    = 'image_file/effect/UI/awakening.png'
+
