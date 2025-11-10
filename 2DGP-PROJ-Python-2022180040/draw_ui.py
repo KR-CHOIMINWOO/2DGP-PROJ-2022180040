68d7013953b313917cbac1216e7247c310e58675
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-11-11 00:24:16 +0900

    draw 함수 완성 but 출력 안됨 어디가 문제일까?

--- 2DGP-PROJ-Python-2022180040/draw_ui.py
+++ 2DGP-PROJ-Python-2022180040/draw_ui.py
@@ -49,10 +49,51 @@ def draw_hp_bar():
     frame_x = x0 + icon_size + 80
     frame_y = y0 - 12
 
+    ratio = min(1.0, max(0.0, hp / float(hp_max)))
+
+    fw, fh = img_hp_fill.w, img_hp_fill.h
+
+    clip_w = int(fw * ratio)
+    if clip_w > 0:
+        img_hp_fill.clip_draw(0, 0, clip_w, fh, frame_x - frame_w/2 + frame_w*ratio/2, frame_y, frame_w * ratio, frame_h)
+    img_hp_frame.draw(frame_x, frame_y, frame_w, frame_h)
+
 
 def draw_skill_icon():
-    pass
+    cw, ch = get_canvas_width(), get_canvas_height()
+    pad = 16
+    size = 72
+
+    roll_x = cw - pad - size / 2
+    roll_y = pad + size / 2
+    special_x = cw - pad * 2 - size * 1.5
+    special_y = pad + size / 2
+
+    def draw_with_cd(img, x, y, w, h, cd, cd_max):
+        if cd <= 0.0:
+            img.opacify(1.0)
+            img.draw(x, y, w, h)
+        else:
+            img.opacify(0.45)
+            img.draw(x, y, w, h)
+            img.opacify(1.0)
+
+            k = max(0.0, min(1.0, cd / cd_max))
+
+            img.opacify(0.25 + 0.35 * k)
+            img.draw(x, y, w, h)
+            img.opacify(1.0)
+
+
+    draw_with_cd(img_btn_roll, roll_x, roll_y, size, size, roll_cooltime, roll_cooltime_max)
+
+    draw_with_cd(img_awaken, special_x, special_y, size, size, special_cooltime, special_cooltime_max)
+    if special_active and special_cooltime <= 0.0:
+        img_awaken.opacify(0.22)
+        img_awaken.draw(special_x, special_y, int(size * 1.06), int(size * 1.06))
+        img_awaken.opacify(1.0)
 
 def draw():
-    pass
+    draw_hp_bar()
+    draw_skill_icon()
 
