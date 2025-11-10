399ddb72d322a71d26361b2d1dc37884c1ff7670
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-11-10 23:55:00 +0900

    이미지를 load하고 후에 용사 체력, 쿨타임 등을 업데이트하기 위한 함수 생성

--- 2DGP-PROJ-Python-2022180040/draw_ui.py
+++ 2DGP-PROJ-Python-2022180040/draw_ui.py
@@ -11,3 +11,17 @@ IMG_ICON_TUAR = 'image_file/effect/UI/icon_tuar.png'
 IMG_BTN_ROLL  = 'image_file/effect/UI/button_roll.png'
 IMG_AWAKEN    = 'image_file/effect/UI/awakening.png'
 
+def init():
+    global img_hp_fill, img_hp_frame, img_hp_text
+    global img_icon_tuar, img_btn_roll, img_awaken
+
+    img_hp_fill   = load_image(IMG_HP_FILL)
+    img_hp_frame  = load_image(IMG_HP_FRAME)
+    img_hp_text   = load_image(IMG_HP_TEXT)
+    img_icon_tuar = load_image(IMG_ICON_TUAR)
+    img_btn_roll  = load_image(IMG_BTN_ROLL)
+    img_awaken    = load_image(IMG_AWAKEN)
+
+def update():
+    pass
+
