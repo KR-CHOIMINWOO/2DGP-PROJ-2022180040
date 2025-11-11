2c43874eda2390fef0eb66b05ba5282bd69bac6b
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-11-12 00:09:55 +0900

    door 부여

--- 2DGP-PROJ-Python-2022180040/make_dungeon.py
+++ 2DGP-PROJ-Python-2022180040/make_dungeon.py
@@ -12,15 +12,19 @@ class Wall:
     def handle_collision(self, group, other):
         pass
 class Door:
-    def __init__(self, l, b, r, t):
+    def __init__(self, l, b, r, t, name, on_enter):
         self.l, self.b, self.r, self.t = l, b, r, t
+        self.name = name
+        self.on_enter = on_enter
     def update(self): pass
     def draw(self):
-        draw_rectangle(self.l, self.b, self.r, self.t)
+        # draw_rectangle(self.l, self.b, self.r, self.t)
+        pass
     def get_bb(self):
         return (self.l, self.b, self.r, self.t)
     def handle_collision(self, group, other):
-        pass
+        if group == 'tuar:door':
+            self.on_enter(self.name, other)
 
 class Dungeon:
     def __init__(self):
@@ -30,6 +34,7 @@ class Dungeon:
         self.y = self.h //2
         self.play_x1, self.play_y1 = 130, 100
         self.play_x2, self.play_y2 = 900, 600
+
         top = (0, self.play_y2, self.w, self.h)
         left = (0, 0, self.play_x1, self.h)
         bottom = (0, 0, self.w, self.play_y1)
@@ -41,6 +46,19 @@ class Dungeon:
             Wall(*bottom),
             Wall(*right),
         ]
+
+        self.door_thick = 24  # 감지 폭
+        self.door_lines = [
+            # name, (ax, ay) ~ (bx, by)
+            ('top',    450, 120, 580, 120),
+            ('right',  880, 300, 880, 400),
+            ('bottom', 450, 580, 580, 580),
+            ('left',   150, 300, 150, 400),
+        ]
+        self.doors = []
+        self._build_doors()
+
+        self.room_id = 0
 
     def update(self):
         pass
