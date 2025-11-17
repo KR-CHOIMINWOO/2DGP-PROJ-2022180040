b49c796bcf627d43c016ce8cb602e16d0ab97856
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-11-18 00:24:58 +0900

    몹 관련 클래스 생성, 필요 기본 함수 미리 선언

---
+++ 2DGP-PROJ-Python-2022180040/mob.py
@@ -0,0 +1,64 @@
+from pico2d import load_image, draw_rectangle, get_canvas_width, get_canvas_height
+import  game_framework
+import game_world
+import play_mode
+
+class Monster:
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
+    def get_bb(self):
+        pass
+    def handle_collision(self, group, other):
+        pass
+
+class DeathKnight(Monster):
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
+    def get_bb(self):
+        pass
+    def handle_collision(self, group, other):
+        pass
+
+class Ghoul(Monster):
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
+    def get_bb(self):
+        pass
+    def handle_collision(self, group, other):
+        pass
+
+class Grave(Monster):
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
+    def get_bb(self):
+        pass
+    def handle_collision(self, group, other):
+        pass
+
+class Zombie(Monster):
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
+    def get_bb(self):
+        pass
+    def handle_collision(self, group, other):
+        pass
