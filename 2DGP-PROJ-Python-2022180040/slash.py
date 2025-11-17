048f5e43c875a5fccf1793f14e0a3153a03643cf
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-11-17 16:11:10 +0900

    init 저장

--- 2DGP-PROJ-Python-2022180040/slash.py
+++ 2DGP-PROJ-Python-2022180040/slash.py
@@ -9,9 +9,19 @@ SLASH_IMG_PATH1    = 'image_file/effect/s1_01.png'
 SLASH_IMG_PATH2    = 'image_file/effect/s1_02.png'
 
 class Slash:
-    def __init__(self):
+    def __init__(self, x, y, face_dir):
+        self.x = x
+        self.y = y
+        self.dir = 1 if face_dir >= 0 else -1
+        self.vx = self.dir * SLASH_SPEED_PPS
+        self.vy = 0.0
+        self.alive_dist = 0.0
+        try:
+            self.image = load_image(SLASH_IMG_PATH1)
+        except:
+            self.image = None
         pass
-    def update(self)
+    def update(self):
         pass
     def draw(self):
         pass
