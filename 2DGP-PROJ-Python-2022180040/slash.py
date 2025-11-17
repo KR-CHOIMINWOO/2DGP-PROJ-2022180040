a4717e020803961ceffa869498178ef89a5b6b49
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-11-17 16:08:28 +0900

    검기를 위한 변수 선언

---
+++ 2DGP-PROJ-Python-2022180040/slash.py
@@ -0,0 +1,9 @@
+from pico2d import draw_rectangle, load_image
+import game_framework
+
+SLASH_SPEED_PPS   = 700.0
+SLASH_RANGE       = 320.0
+SLASH_THICK       = 20
+SLASH_W           = 60
+SLASH_IMG_PATH1    = 'image_file/effect/s1_01.png'
+SLASH_IMG_PATH2    = 'image_file/effect/s1_02.png'
