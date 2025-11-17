32e5ec8b751cc58b25ea9197b051a3a74c0e1c9a
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-11-17 16:20:57 +0900

    그림 수정, 그냥 하나만 사용하고 회전, 플립을 통해 그리기

--- 2DGP-PROJ-Python-2022180040/slash.py
+++ 2DGP-PROJ-Python-2022180040/slash.py
@@ -6,8 +6,7 @@ SLASH_SPEED_PPS   = 700.0
 SLASH_RANGE       = 360.0
 SLASH_W           = 50
 SLASH_H           = 90
-SLASH_IMG_PATH1    = 'image_file/effect/s1_01.png'
-SLASH_IMG_PATH2    = 'image_file/effect/s1_02.png'
+SLASH_IMG_PATH    = 'image_file/effect/s1_01.png'
 
 class Slash:
     def __init__(self, x, y, face_dir):
@@ -18,15 +17,25 @@ class Slash:
         self.vy = 0.0
         self.alive_dist = 0.0
         try:
-            self.image = load_image(SLASH_IMG_PATH1 if self.dir > 0 else SLASH_IMG_PATH2)
+            self.image = load_image(SLASH_IMG_PATH)
         except:
             self.image = None
         pass
     def update(self):
-        pass
+        dt = game_framework.frame_time
+        dx = self.vx * dt
+        dy = self.vy * dt
+        self.x += dx
+        self.y += dy
+        self.alive_dist += (dx * dx + dy * dy) ** 0.5 + abs(dy)
+
+        if self.alive_dist >= SLASH_RANGE:
+            game_world.remove_object(self)
     def draw(self):
         pass
     def get_bb(self):
-        pass
+        return (self.x - SLASH_W // 2, self.y - SLASH_H // 2,
+                self.x + SLASH_W // 2, self.y + SLASH_H // 2)
+
     def handle_collision(self, group, other):
         pass
