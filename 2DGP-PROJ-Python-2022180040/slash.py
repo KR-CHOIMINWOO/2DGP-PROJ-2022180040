191e508a40883f9b1f5004cd9a01f2170ba963cb
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-11-18 23:58:10 +0900

    slash에 조건들 추가

--- 2DGP-PROJ-Python-2022180040/slash.py
+++ 2DGP-PROJ-Python-2022180040/slash.py
@@ -12,10 +12,14 @@ SLASH_IMG_PATH    = 'image_file/effect/s1_01.png'
 DIR_RIGHT, DIR_LEFT, DIR_UP, DIR_DOWN = 'right', 'left', 'up', 'down'
 
 class Slash:
-    def __init__(self, x, y, direction: str):
+    def __init__(self, x, y, direction: str, owner=None):
         self.x, self.y = x, y
         self.direction = direction
         self.alive_dist = 0.0
+
+        base_atk = getattr(owner, 'atk', 1) if owner else 1
+        mult = 2 if getattr(owner, 'special_active', False) else 1
+        self.damage = base_atk * mult
 
         if direction == DIR_RIGHT:
             self.vx, self.vy = SLASH_SPEED_PPS, 0.0
@@ -31,13 +35,20 @@ class Slash:
             self.flip = ''
         else:
             self.vx, self.vy = 0.0, -SLASH_SPEED_PPS
-            self.angle = -math.pi / 2  #
+            self.angle = -math.pi / 2
             self.flip = ''
 
         try:
             self.image = load_image(SLASH_IMG_PATH)
         except:
             self.image = None
+
+    def is_in_world(self):
+        for layer in game_world.world:
+            if self in layer:
+                return True
+        return False
+
     def update(self):
         dt = game_framework.frame_time
         dx = self.vx * dt
@@ -47,16 +58,23 @@ class Slash:
         self.alive_dist += (dx * dx + dy * dy) ** 0.5 + abs(dy)
 
         if self.alive_dist >= SLASH_RANGE:
-            game_world.remove_object(self)
+            if self.is_in_world():
+                game_world.remove_object(self)
+
     def draw(self):
         if self.image:
-            self.image.composite_draw(self.angle, self.flip, self.x, self.y, SLASH_W*1.2, SLASH_H*1.1)
+            self.image.composite_draw(self.angle, self.flip, self.x, self.y, SLASH_W * 1.2, SLASH_H * 1.1)
         else:
-            draw_rectangle(self.x - SLASH_W//2, self.y - SLASH_H//2,
-                           self.x + SLASH_W//2, self.y + SLASH_H//2)
+            draw_rectangle(self.x - SLASH_W // 2, self.y - SLASH_H // 2,
+                           self.x + SLASH_W // 2, self.y + SLASH_H // 2)
+
     def get_bb(self):
         return (self.x - SLASH_W // 2, self.y - SLASH_H // 2,
                 self.x + SLASH_W // 2, self.y + SLASH_H // 2)
 
     def handle_collision(self, group, other):
-        pass
+        if group == 'slash:monster':
+            if hasattr(other, 'take_damage'):
+                other.take_damage(self.damage)
+            if self.is_in_world():
+                game_world.remove_object(self)
