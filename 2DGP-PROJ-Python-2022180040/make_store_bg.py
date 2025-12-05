dee0941e9d908b635762ecf77c6d06500bab416f
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-12-05 21:31:12 +0900

    store용 bg 파일 생성

---
+++ 2DGP-PROJ-Python-2022180040/make_store_bg.py
@@ -0,0 +1,13 @@
+from pico2d import load_image
+
+
+class Bg:
+    def __init__(self):
+        self.image = load_image('image_file/bag/wait_bg2.png')
+
+    def draw(self):
+        self.image.draw(1024 / 3, 720 / 2, 1024, 720)
+        self.image.draw(1024 , 720 / 2, 1024, 720)
+
+    def update(self):
+        pass
