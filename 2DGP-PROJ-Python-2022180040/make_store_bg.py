67b11617f0beab3a4772da810c9a279bfdfff8c6
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-12-06 16:24:04 +0900

    그림 변경

--- 2DGP-PROJ-Python-2022180040/make_store_bg.py
+++ 2DGP-PROJ-Python-2022180040/make_store_bg.py
@@ -3,10 +3,10 @@ from pico2d import load_image
 
 class Bg:
     def __init__(self):
-        self.image = load_image('image_file/bag/store_room_in.png')
+        self.image = load_image('image_file/bag/store_room_bg.png')
 
     def draw(self):
-        self.image.draw(1024 / 2 - 20 , 720 / 2 + 60  , 1024, 1024)
+        self.image.draw(1024 / 2 - 20 , 720 / 2 - 60   , 1024, 1024)
 
     def update(self):
         pass
